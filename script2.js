c17fbe1cf9677258e7ad15e79e8f6b84cba5790c
AliyaAibekova <88720145+Aliiaaibekova@users.noreply.github.com>
2022-01-29 02:42:35 +0600

    Unit 1

---
+++ script2.js
@@ -0,0 +1,5 @@
+//ASK 2
+let user = "Aliya"
+console.log(user)
+user="Leya"
+console.log(user)
---
+++ script4.js
@@ -0,0 +1,7 @@
+// ASK 4
+let souv = prompt("Введите количество сувенира");
+let trin = prompt("Введите количество безделушек");
+const souvenir = 75;
+const trinket = 112;
+let genWeight = souv * souvenir+ trin* trinket;
+document.write("Общий вес посылок:"+ genWeight+"грамм <br>");
