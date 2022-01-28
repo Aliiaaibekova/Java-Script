9e8c5e512a48ccaa965d1c948cc33b8d35d43116
AliyaAibekova <88720145+Aliiaaibekova@users.noreply.github.com>
2022-01-29 03:09:23 +0600

    Unit 1

---
+++ Unit1/script3.js
@@ -0,0 +1,9 @@
+//ASK 3
+let userName = prompt ("Ваше имя: ?");
+let lastName = prompt ("Фамилия: ?")
+let emailName = prompt ("Ваше e-mail: ?");
+
+alert("Привет."+"Добро пожаловать!");
+document.write("<b> Ваше имя: </b>" + userName +"<br>");
+document.write("<b> Фамилия: </b>" +  lastName +"<br>");
+document.write("<b> Ваше e-mail: </b>" + emailName +"<br>");
